ec20ae7ad6d1032c55c85b4ec3fe4029f669093d
Krishal8957 <111569458+krishal-1701@users.noreply.github.com>
2023-10-13 18:41:11 +0530

    Add files via upload

---
+++ PBL/main.js
@@ -0,0 +1,18 @@
+// Table Hovering
+
+const enlargeImages = document.querySelectorAll('.tabimg');
+
+function handleImageHover(event) {
+   event.target.style.transform = 'scale(1.2)'; // You can adjust the scale factor as per your preference
+}
+
+
+function handleImageMouseOut(event) {
+    event.target.style.transform = 'scale(1)';
+}
+
+enlargeImages.forEach(image => {
+    
+    image.addEventListener('mouseover', handleImageHover);
+    image.addEventListener('mouseout', handleImageMouseOut);
+});
