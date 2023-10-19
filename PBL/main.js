8e192e7c54b1d15c65cca5106e857da622c33887
VishwajeetMuthe1970 <93135572+VishwajeetMuthe1970@users.noreply.github.com>
2023-10-20 00:48:58 +0530

    Add files via upload

--- PBL/main.js
+++ PBL/main.js
@@ -1,16 +1,22 @@
-//    Appearing text    
+// Appearing Text
+function animateTextAppearance() {
+    var textElements = document.querySelectorAll('.centerbelowtext');
+    textElements.forEach(element => {
+        const text = element.innerText;
+        element.innerHTML = '';
+        for (let i = 0; i < text.length; i++) {
+            const letter = text[i];
+            const span = document.createElement('span');
+            span.textContent = letter;
+            span.className = 'hidden';
+            element.appendChild(span);
+            span.style.animation = `appear 0.1s forwards ${i * 0.12}s`;
+        }
+    });
+}
 
-var textElements = document.querySelectorAll('.centerbelowtext');
-        textElements.forEach(element => {
-            const text = element.innerText;
-            element.innerHTML = '';
-            for (let i = 0; i < text.length; i++) 
-            {
-                const letter = text[i];
-                const span = document.createElement('span');
-                span.textContent = letter;
-                span.className = 'hidden';
-                element.appendChild(span);
-                span.style.animation = `appear 0.1s forwards ${i * 0.1}s`;
-            }
-        });
+// Call the function initially
+animateTextAppearance();
+
+// Repeat the animation every 1 second (1000 milliseconds)
+setInterval(animateTextAppearance, 16500);
