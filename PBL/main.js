289c12b48d4ae51b4291a3018506b53798d69252
Krishal8957 <111569458+krishal-1701@users.noreply.github.com>
2023-10-19 20:35:08 +0530

    Add files via upload

--- PBL/main.js
+++ PBL/main.js
@@ -1,19 +1,20 @@
-// Table Hovering
+//    Appearing text    
 
-const enlargeImages = document.querySelectorAll('.tabimg');
-
-function handleImageHover(event) {
-   event.target.style.transform = 'scale(1.2)'; // You can adjust the scale factor as per your preference
-}
-
-
-function handleImageMouseOut(event) {
-    event.target.style.transform = 'scale(1)';
-}
-
-enlargeImages.forEach(image => {
-    
-    image.addEventListener('mouseover', handleImageHover);
-    image.addEventListener('mouseout', handleImageMouseOut);
-});
-
+var textElements = document.querySelectorAll('.centerbelowtext');
+        textElements.forEach(element => {
+            const text = element.innerText;
+            element.innerHTML = '';
+            for (let i = 0; i < text.length; i++) 
+            {
+                const letter = text[i];
+                const span = document.createElement('span');
+                span.textContent = letter;
+                span.className = 'hidden';
+                element.appendChild(span);
+                span.style.animation = `appear 0.1s forwards ${i * 0.1}s`;
+                if(i == text.length)
+                {
+                    i=0;
+                }
+            }
+        });
