a83b2e61b8ee7e2d46c01d0c083544a447cf1fa5
Krishal8957 <111569458+krishal-1701@users.noreply.github.com>
2023-12-27 21:19:21 +0530

    Add files via upload

--- PBL/main.js
+++ PBL/main.js
@@ -1,3 +1,12 @@
+// Splash Page
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.getElementById('loading-page').style.opacity = '0';
+    }, 2000); // Adjust the delay in milliseconds (e.g., 3000ms = 3 seconds)
+  });
+
+
+
 // Appearing Text
 function animateTextAppearance() {
     var textElements = document.querySelectorAll('.centerbelowtext');
@@ -10,7 +19,7 @@ function animateTextAppearance() {
             span.textContent = letter;
             span.className = 'hidden';
             element.appendChild(span);
-            span.style.animation = `appear 0.1s forwards ${i * 0.12}s`;
+            span.style.animation = `appear 0.1s forwards ${i * 0.18}s`;
         }
     });
 }
@@ -20,3 +29,4 @@ animateTextAppearance();
 
 // Repeat the animation every 1 second (1000 milliseconds)
 setInterval(animateTextAppearance, 16550);
+
